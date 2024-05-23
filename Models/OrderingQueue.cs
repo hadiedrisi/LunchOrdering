3cf4a54c83f3158310f86f9ffb1f637b08abdd43
hadi-hedco <edrisi.hadi@hedcoint.com>
2024-05-23 10:19:07 +0330

    create required models

---
+++ Models/OrderingQueue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchOrderingSystem.Models
+{
+    public enum Status
+    {
+        Waiting,
+        Ibnprogtress,
+        Done,
+    }
+    public class OrderingQueue
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public int OrderId { set; get; }
+        public int ProductionId { set; get; }
+        public string ProductionName { set; get;}
+        public Status status { set; get; }
+    }
+}
---
+++ Models/ProductionUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchOrderingSystem.Models
+{
+    public class ProductionUnit
+    {
+        public ProductionUnit() { }
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Type { set; get; }
+        public bool IsAvailable { set; get; }
+
+    }
+}
